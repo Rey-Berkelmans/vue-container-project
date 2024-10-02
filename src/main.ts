91aee5d0f73efd798535e3276c1d3a46a70492ca
Rey-Berkelmans <appledragonpokemon@gmail.com>
2024-10-02 11:02:49 +1000

    Main.ts Update

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,18 @@
+// import './assets/main.css'
+
+// import { createApp } from 'vue'
+// import { createPinia } from 'pinia'
+
+// import App from './App.vue'
+// import router from './router'
+
+// const app = createApp(App)
+
+// app.use(createPinia())
+// app.use(router)
+
+// app.mount('#app')
+
 import './assets/main.css'
 
 import { createApp } from 'vue'
@@ -7,8 +22,9 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
