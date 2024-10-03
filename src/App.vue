c2b473dec53e8a4ee2dc599dc5d6353d767b49b4
Rey-Berkelmans <appledragonpokemon@gmail.com>
2024-10-03 17:47:11 +1000

    Minor Update

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <header>
-    <!-- <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
-    <!-- Vue Logo -->
+    <!-- <img alt="Icon" class="logo" src="@/assets/logo.svg" width="125" height="125" /> -->
 
     <div class="wrapper">
       <HelloWorld msg="Explore Home Movies" />
